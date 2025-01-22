24791ec6855a9b4534aaa02df053db91e50cf765
Rithvik-Chikatla <98206714+Rithvik-Chikatla@users.noreply.github.com>
2025-01-22 23:43:15 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -11,12 +11,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-
-db_pass = os.getenv('db_pass')
 
 URL_DATABASE = 'mysql+pymysql://username:password@localhost:3306/job_portal'
 
