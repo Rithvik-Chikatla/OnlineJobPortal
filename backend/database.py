d5c17a5398ea0f812017b529c72af58208d212c7
Rithvik-Chikatla <98206714+Rithvik-Chikatla@users.noreply.github.com>
2025-01-22 23:42:52 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 db_pass = os.getenv('db_pass')
 
-URL_DATABASE = 'mysql+pymysql://root:rithvik04@localhost:3306/job_portal'
+URL_DATABASE = 'mysql+pymysql://username:password@localhost:3306/job_portal'
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
